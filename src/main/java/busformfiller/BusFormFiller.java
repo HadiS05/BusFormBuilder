8b0fd9ec9babce00de33a307f2ba9775c026593f
Hadi Shaheryar <140560253+HadiS05@users.noreply.github.com>
2023-10-03 19:39:23 -0400

    Updated the PDF so that it could be parsed as an AcroForm, Successfully getting PDF Fields now

--- src/main/java/busformfiller/BusFormFiller.java
+++ src/main/java/busformfiller/BusFormFiller.java
@@ -135,7 +135,6 @@ public class BusFormFiller {
 			prestoArray.printArray();
 			
 			PDFFiller pdfiller = new PDFFiller();
-			pdfiller.fill();
 		}catch(DateTimeException e) { // Catch a date error due to improper user input
 			e.printStackTrace();
 		}catch(IOException r) {
